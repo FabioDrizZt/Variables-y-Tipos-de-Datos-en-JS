4ad283c7cc9933862cc81b84f6a9c5ec7ccea148
Fabi0x <Fabi0x@argentina programa.com>
2023-08-03 20:26:32 -0300

    Agregue Tipos de datos

---
+++ Tipos de datos.js
@@ -0,0 +1,37 @@
+//number o numericos
+let numeroPositivo = 123
+let numeroNegativo = -123
+let numeroDecimal = 1.23
+let numeroDecimalNeg = -1.23
+
+//strings o cadenas de texto
+let cadenaSimple = 'soy una "cadena" simple'
+let cadenadoble = "soy una 'cadena' doble"
+let frase = "Mi \"amigo\" D'alessandro es un futbolista" // Alt + 92 = \
+console.log(frase) 
+let nombre = "Fabio"
+let saludo = `Hola ${nombre}` // Alt + 96 = `
+console.log(saludo)
+
+// booleanos 
+let verdadero = true
+let falso = false
+console.log(verdadero)
+console.log(falso)
+
+// NaN --> Not a Number
+console.log(1-"fabio")
+console.log(1/"fabio")
+console.log(-1/0)
+console.log(1/0)
+
+//undefined
+let variableIndefinida
+console.log(variableIndefinida)
+variableIndefinida="definida"
+console.log(variableIndefinida)
+//null
+let variableNula = null
+console.log(variableNula)
+variableNula = 12
+console.log(variableNula)
