4f220f94d08cd5d8e2828898695c8c2d1341d700
Fabi0x <Fabi0x@argentina programa.com>
2023-08-03 19:29:12 -0300

    AP - Clase 2 - JS

---
+++ Operadores Aritmeticos.js
@@ -0,0 +1,30 @@
+let suma = 8+7
+console.log(suma)
+
+let resta = 8-7
+console.log(resta)
+
+let multiplicacion = 8*7
+console.log(multiplicacion)
+
+let division = 8/7
+console.log(division)
+
+let modulo = 14%4
+console.log(modulo)
+
+let incrementadora = 1
+// pre incremento 
+console.log(++incrementadora) // 2
+// post incremento 
+console.log(incrementadora++) // muestra 2 pero vale 3
+console.log(incrementadora) // muestra 3
+
+let decrementadora = 1
+// pre decremento 
+console.log(--decrementadora) // 0
+// post decremento 
+console.log(decrementadora--) // muestra 0 pero vale -1
+console.log(decrementadora) // muestra -1
+
+decrementadora = decrementadora-1
